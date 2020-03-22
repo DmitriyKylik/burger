3260c3b2703eaa4cc203d9404744e64eb33804c5
Kylik Dmitriy <BkForest20@gmail.com>
2020-03-22 20:25:54 +0200

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+// import App from "./components/App.js";
+
+// JS
+// import './js/'
+// SCSS
+// import './assets/scss/main.scss'
+
+// CSS (example)
+import './assets/css/main.css'
+
+ReactDOM.render(<App />, document.getElementById("root"));
