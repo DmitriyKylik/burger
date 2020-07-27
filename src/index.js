525bc3be852e5e33aff74d4a90ca4ce1c1bd7500
Kylik Dmitriy <BkForest20@gmail.com>
2020-07-27 14:11:49 +0300

    Add tab to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
 import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
